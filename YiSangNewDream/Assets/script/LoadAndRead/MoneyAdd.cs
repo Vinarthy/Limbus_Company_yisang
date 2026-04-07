ea03b7c6bff358b35895707eb07ecb02c9fb9c06
Vinarthy <2521544542@qq.com>
2026-04-07 09:28:32 +0800

    上传了新UI

---
+++ YiSangNewDream/Assets/script/LoadAndRead/MoneyAdd.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyAdd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //这个脚本挂到通讯中枢上，负责管money的增减
+    //money的存储是场景里面那个UI上有一个然后有一个json，按了存储才能作为个档位存进去
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
